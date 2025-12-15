f92b9c788fcc7cd688e874a333715a93b161762c
Bill Tallitsch <1752682+btallitsch@users.noreply.github.com>
2025-12-15 18:11:13 -0500

    Implement AppNavigator with stack navigation

---
+++ src/navigation/AppNavigator.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { RootStackParamList } from '../types';
+import { CourtsListScreen } from '../screens/CourtsListScreen';
+import { CourtDetailScreen } from '../screens/CourtDetailScreen';
+
+const Stack = createNativeStackNavigator();
+
+export const AppNavigator = () => {
+  return ();
+};
